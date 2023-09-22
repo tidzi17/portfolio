dc27f04619673b94b7402c369a4ea1707318b907
tidzi17 <tidzi15110@gmail.com>
2023-09-23 00:12:55 +0200

    responsive

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,34 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { useState} from "react";
+/* import { Link } from 'react-router-dom'; */
+import { useNavigate } from "react-router-dom";
+import {FaBars} from 'react-icons/fa';
+import {ImCross} from 'react-icons/im'
+ 
 import '../styles/main.scss'
 
 function Navbar() {
+  const navigate = useNavigate();
+  const [Mobile, setMobile] = useState(false);
+
+
+
     return (
+      <header>
         <div className="navigation">
-      <div className="navbar">
-        <ul className="nav-links">
-          <li><Link to="/" className="nav-link">Home</Link></li>
-          <li><Link to="/about" className="nav-link">About</Link></li>
-          <li><Link to="/portfolio" className="nav-link">Portfolio</Link></li>
-          <li><Link to="/contact" className="nav-link">Contact</Link></li>
+      <nav className="navbar">
+        <ul className={Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}>
+          <li className="nav-item" onClick={() => navigate("/")}>Home</li>
+          <li className="nav-item" onClick={() => navigate("/about")}>About</li>
+          <li className="nav-item" onClick={() => navigate("/portfolio")}>Portfolio</li>
+          <li className="nav-item" onClick={() => navigate("/contact")}>Contact</li>
         </ul>
+        <button className="mobile-menu-icon" onClick={() => setMobile(!Mobile)}>
+        {Mobile ? <ImCross /> : <FaBars />}
+        </button>
+      </nav>
       </div>
-      </div>
+      </header>
     );
   }
   
